f12b78eec277201614f3bc98329992553848a523
Ale_Cipriano <alessandro_cipriano@hotmail.com>
2024-03-18 18:21:08 -0300

    Criação da Result Activity

--- app/src/main/java/com/example/androidotsintroale/MainActivity.kt
+++ app/src/main/java/com/example/androidotsintroale/MainActivity.kt
@@ -1,57 +1,37 @@
 package com.example.androidotsintroale
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-/* class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-     /*   ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        } */
-
-        val btnEnviar = findViewById<Button>(R.id.btn_enviar)
-        val txtResult = findViewById<TextView>(R.id.txt_result)
-
-        btnEnviar.setOnContextClickListener {
-            txtResult.text = "Alessandro Cipriano Gonzaga"
-        }
-    }
-}
-*/
 
 class MainActivity : AppCompatActivity() {
-    // @SuppressLint("MissingInflatedId")
+    @SuppressLint("MisingInlfatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+            setContentView(R.layout.activity_main)
 
         // Botão de envio de informações
         val btnEnviar = findViewById<Button>(R.id.btn_enviar)
         // Exibir informações
-        val txtResult = findViewById<TextView>(R.id.txt_result)
+        val txtResult  = findViewById<TextView>(R.id.txt_result)
         // Inserir informações
-        // val edText = findViewById<EditText>(R.id.edText)
-        // Abrir nova tela
-        // val abrirNovaTela = findViewById<Button>(R.id.btnNovaTela)
+        val edText = findViewById<EditText>(R.id.edText)
 
-        // Variável que armazena informaões inseridas
-        // val inputName = edText.text
+        val inputName = edText.text
 
         // Botão de envio de informações
-        btnEnviar.setOnClickListener {
-        txtResult.text = "Alessandro Cipriano Gonzaga"
+              btnEnviar.setOnClickListener {
 
+                  if(inputName.isBlank()) {
+                      edText.error = "Este campo não pode ser vazio"
+                  }else {
+
+                      txtResult.text = edText.text
+                  }
         }
     }
 }
